f413986f12c92e6629f2afd94a134c8d5d8a14d2
Septiandry Khrisna <septiandryk@gmail.com>
2022-06-11 22:48:28 +0700

    Adding cosmetic, exit, & tutorial

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -8,13 +8,19 @@ public class MainMenuController : MonoBehaviour
     
     public void PlayGame() 
     { 
-        SceneManager.LoadScene("PongGame"); 
+        SceneManager.LoadScene("Tutorial"); 
     } 
 
-        public void OpenAuthor() 
+    public void OpenAuthor() 
     { 
         Debug.Log("Created By Septiandry Khrisna Setiono - 149251970101-54"); 
-    } 
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
 
     // Start is called before the first frame update
     void Start()
